8cc06f6016ff4a9d1537f48f98ec938cff439d04
KarlTheFish <KarlTheFish@users.noreply.github.com>
2023-09-13 12:49:18 +0300

    Start of lesson 13.09.2023

---
+++ Tunnitöö/13.09.2023/basics.js
@@ -0,0 +1,15 @@
+const test = [12, 21, 53, 23, 2];
+
+function findIndex(ar, nr){
+    return test.indexOf(nr);
+}
+
+//console.log(findIndex(test, 2));
+
+//arrow function
+
+function addition(nr1, nr2){
+    return nr1 + nr2;
+}
+
+console.log(addition(221, 463));
